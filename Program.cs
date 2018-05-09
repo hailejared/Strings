0962b6cafa1d06c0315d59596b2c54174efa8ee8
Jared <Jared.o@live.com>
2018-05-09 12:31:18 +1200

    Commit All.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = '\'';
+            Console.WriteLine(ch);
+            Console.WriteLine();
+
+            string str = @"""Hello""";
+            Console.WriteLine(str);
+            Console.WriteLine();
+
+            string str1 = "\"Hello\nWorld\"";
+            Console.WriteLine(str1);
+            Console.WriteLine();
+
+            string str2 = "Hello\\World";
+            Console.WriteLine(str2);
+            Console.WriteLine();
+
+            string str3 = "Hello\0World\0";
+            Console.WriteLine(str3);
+            Console.WriteLine();
+
+            string str4 = "Alert Sound:\a";
+            Console.WriteLine(str4);
+            Console.WriteLine();
+
+            string str5 = "Back\bspace";
+            Console.WriteLine(str5);
+            Console.WriteLine();
+
+            string str6 = "Tab\tTab";
+            Console.WriteLine(str6);
+            Console.WriteLine();
+        }
+    }
+}
